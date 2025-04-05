4baeea670a3a8a42bd108f1fb50a8ccd79705944
Samwel Mpuyuk <148690527+Sammy0254@users.noreply.github.com>
2025-04-05 20:29:40 +0300

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -43,7 +43,7 @@
       <div class="panel-heading">
         <strong>
           <span class="glyphicon glyphicon-th"></span>
-          <span>Add New User</span>
+          <span>Add A New User</span>
        </strong>
       </div>
       <div class="panel-body">
